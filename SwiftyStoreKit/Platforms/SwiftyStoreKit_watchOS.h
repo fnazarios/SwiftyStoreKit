23fd7b156c2c38d6ecffca9255c96c18ecf684f1
Fernando Nazario <fnazarios@gmail.com>
2020-03-03 20:52:47 -0300

    Adding watchOS Compatibility

---
+++ SwiftyStoreKit/Platforms/SwiftyStoreKit_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyStoreKit_watchOS.h
+//  SwiftyStoreKit_watchOS
+//
+//  Created by Fernando Sousa on 03/03/20.
+//  Copyright © 2020 musevisions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyStoreKit_watchOS.
+FOUNDATION_EXPORT double SwiftyStoreKit_watchOSVersionNumber;
+
+//! Project version string for SwiftyStoreKit_watchOS.
+FOUNDATION_EXPORT const unsigned char SwiftyStoreKit_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyStoreKit_watchOS/PublicHeader.h>
+
+
